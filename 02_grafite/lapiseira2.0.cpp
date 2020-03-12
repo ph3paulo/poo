b25280a6edc9296705c8db0d327df179b7343bfd
ph3paulo <ph3.paulotec@gmail.com>
2020-03-12 19:39:34 -0300

    ultima atividade

--- 02_grafite/lapiseira2.0.cpp
+++ 02_grafite/lapiseira2.0.cpp
@@ -12,80 +12,106 @@ struct Lapiseira{
 
     void colocar_ponta(Ponta * ponta){
         if(this->ponta != nullptr)
-            cout << "ja tem ponta na lapiseira" << "\n";
-        else
+            cout << ">>ja tem ponta na lapiseira" << "\n";
+        else{
             this->ponta = ponta;
+            cout << ">>ponta colocada" << "\n";
+        }
+    }
+    void retira_ponta(){
+        this->ponta = nullptr;
+        cout << ">>ponta retirada";
     }
 
     void recarregar(){
-        if (ponta->quantidade_ponta <= 99)
-        {
-            for(; ponta->quantidade_ponta <= 100; ponta->quantidade_ponta ++)
-            {
-                ponta->quantidade_ponta += 1;
-            }
-        cout << ">>Regarregado" << "\n";
-
+        if(this->ponta == nullptr){
+            cout << ">>sem ponta" << "\n";
         }else{
-        cout << ">>não precisa recarregar" << "\n";
+            if (ponta->quantidade_ponta <= 99)
+            {
+                for(; ponta->quantidade_ponta <= 100; ponta->quantidade_ponta ++)
+                {
+                    ponta->quantidade_ponta += 1;
+                }
+            cout << ">>Regarregado" << "\n";
+
+            }else{
+            cout << ">>não precisa recarregar" << "\n";
+            }
         }
     }
 
     void escrever(int n){
-        if(ponta->estado_ponta == true){
-            if(n > ponta->quantidade_ponta)
-            {
-            cout << ">>voce não tem grafite suficiente" << "\n";
+        if(this->ponta == nullptr){
+            cout << ">>sem ponta" << "\n";
+        }else{
+            if(ponta->estado_ponta == true){
+                if(n > ponta->quantidade_ponta)
+                {
+                cout << ">>voce não tem grafite suficiente" << "\n";
+                }
+                else if(ponta->quantidade_ponta > 0)
+                {
+                    ponta->quantidade_ponta -= n;
+                    cout << ">>você escreveu ^^" << "\n";
+                }
             }
-            else if(ponta->quantidade_ponta > 0)
-            {
-                ponta->quantidade_ponta -= n;
-                cout << ">>você escreveu ^^" << "\n";
+            else{
+                cout << ">>sua ponta esta guardada não pode escrever" << "\n";
             }
-        }
-        else{
-            cout << ">>sua ponta esta guardada não pode escrever" << "\n";
         }
     }
 
     void ponta_para_fora(){
-        if(ponta->estado_ponta == false)
-        {
-            ponta->estado_ponta = true;
-            cout << ">>sua ponta esta para fora" << "\n";
+        if(this->ponta == nullptr){
+            cout << ">>sem ponta" << "\n";
+        }else{
+            if(ponta->estado_ponta == false)
+            {
+                ponta->estado_ponta = true;
+                cout << ">>sua ponta esta para fora" << "\n";
+            }
+            else
+            {
+                cout << ">>sua ponta ja esta para fora" << "\n";
+            }
+            
         }
-        else
-        {
-            cout << ">>sua ponta ja esta para fora" << "\n";
-        }
-        
     }
 
     void ponta_para_dentro(){
-        if(ponta->estado_ponta == true)
-        {
-            ponta->estado_ponta = false;
-            cout << ">>Sua ponta esta guardada" << "\n";
-        }
-        else
-        {
-            cout << ">>Sua ponta já esta guardada" << "\n";
+        if(this->ponta == nullptr){
+            cout << ">>sem ponta" << "\n";
+        }else{
+            if(ponta->estado_ponta == true)
+            {
+                ponta->estado_ponta = false;
+                cout << ">>Sua ponta esta guardada" << "\n";
+            }
+            else
+            {
+                cout << ">>Sua ponta já esta guardada" << "\n";
+            }
         }
     }
 
     void status(){
-        cout << ">>voce tem essa quantidade: " << ponta->quantidade_ponta << " de grafite" << "\n";
-        if(ponta->quantidade_ponta == true){
-            cout << ">>sua ponta esta para fora" << "\n";
-        }
-        else{
-            cout << ">>Sua ponta esta guardada" << "\n";
+        if(this->ponta == nullptr){
+            cout << ">>sem ponta" << "\n";
+        }else{
+            cout << ">>voce tem essa quantidade: " << ponta->quantidade_ponta << " de grafite" << "\n";
+            if(ponta->quantidade_ponta == true){
+                cout << ">>sua ponta esta para fora" << "\n";
+            }
+            else{
+                cout << ">>Sua ponta esta guardada" << "\n";
+            }
         }
     }
 };
 
 int main(){
-    Lapiseira * lapiseira = new Lapiseira();
+    Lapiseira lapiseira;
     while(true){
         string line;
         getline(cin, line);
@@ -97,17 +123,20 @@ int main(){
         if(cmd == "end"){
             break;
         }else if(cmd == "recarregar"){
-            lapiseira->recarregar();
+            lapiseira.recarregar();
         }else if(cmd == "escrever"){
-            lapiseira->escrever(n);
-        }else if(cmd == "retira_ponta"){
-            lapiseira->ponta_para_fora();
+            lapiseira.escrever(n);
+        }else if(cmd == "sai_ponta"){
+            lapiseira.ponta_para_fora();
         }else if (cmd == "guarda_ponta"){
-            lapiseira->ponta_para_dentro();
+            lapiseira.ponta_para_dentro();
         }else if(cmd == "status"){
-            lapiseira->status();
-        }
-        else{
+            lapiseira.status();
+        }else if(cmd == "colocar_ponta"){
+            lapiseira.colocar_ponta(new Ponta());
+        }else if(cmd == "retira_ponta"){
+            lapiseira.retira_ponta();
+        }else{
             cout << "comando invalido\n";
         }
         cout << "==========================" << "\n";
